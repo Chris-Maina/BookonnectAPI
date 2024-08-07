83771ba77bbeda7a465690b95833f0c852cbd3fb
Chris Maina <mainachrisw@gmail.com>
2024-08-07 07:56:26 +0300

    modify Payment properties

--- BookonnectAPI/Models/Payment.cs
+++ BookonnectAPI/Models/Payment.cs
@@ -4,13 +4,12 @@ public class Payment
 {
 
 	public string ID { get; set; } = string.Empty;// will store the MPESA transaction code
-    public string? Phone { get; set; } // phone number that made payment
     public DateTime DateTime { get; set; }
 
     public int UserID { get; set; } // Required foreign key
-    public User? User { get; set; } = null!; // Optional reference navigation
-    public int OrderID { get; set; } // Required foreign key
-    public Order Order { get; set; } = null!; // Optional reference navigation.
+    public User? User { get; set; } // Optional reference navigation
+    public int? OrderID { get; set; } // Optional foreign key
+    public Order? Order { get; set; } // Optional reference navigation.
 
 }
 
