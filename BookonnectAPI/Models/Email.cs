de9fe76d84a1266790de20fd507a21580d0c374f
Chris Maina <mainachrisw@gmail.com>
2024-10-01 08:16:20 +0300

    add Email

---
+++ BookonnectAPI/Models/Email.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace BookonnectAPI.Models;
+
+public class Email
+{
+	public string ToId { get; set; } = String.Empty;
+	public string Name { get; set; } = String.Empty;
+    public string Subject { get; set; } = String.Empty;
+    public string Body { get; set; } = String.Empty;
+}
+
