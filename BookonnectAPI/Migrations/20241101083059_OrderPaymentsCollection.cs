ecee85b978a9fdd5cf1b395a01d237491e49ab7d
Chris Maina <mainachrisw@gmail.com>
2024-11-05 08:35:13 +0300

    - remove orderID relation in Book - fix userID foreign key in Deliveries - add bookID foreign key in OrderItem - add payments collection in Order.

---
+++ BookonnectAPI/Migrations/20241101083059_OrderPaymentsCollection.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookonnectAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class OrderPaymentsCollection : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Payments_OrderID",
+                table: "Payments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_OrderItem_BookID",
+                table: "OrderItem");
+
+            migrationBuilder.DropColumn(
+                name: "OrderId",
+                table: "Books");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Payments_OrderID",
+                table: "Payments",
+                column: "OrderID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderItem_BookID",
+                table: "OrderItem",
+                column: "BookID",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Deliveries_UserID",
+                table: "Deliveries",
+                column: "UserID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Deliveries_Users_UserID",
+                table: "Deliveries",
+                column: "UserID",
+                principalTable: "Users",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Deliveries_Users_UserID",
+                table: "Deliveries");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Payments_OrderID",
+                table: "Payments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_OrderItem_BookID",
+                table: "OrderItem");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Deliveries_UserID",
+                table: "Deliveries");
+
+            migrationBuilder.AddColumn<int>(
+                name: "OrderId",
+                table: "Books",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Payments_OrderID",
+                table: "Payments",
+                column: "OrderID",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderItem_BookID",
+                table: "OrderItem",
+                column: "BookID");
+        }
+    }
+}
