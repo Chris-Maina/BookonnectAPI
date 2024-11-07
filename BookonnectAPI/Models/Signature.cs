040f9cb5ea88c582c1352b77fce7d72d1677ab43
Chris Maina <mainachrisw@gmail.com>
2024-11-07 23:38:37 +0300

    add Signature table

---
+++ BookonnectAPI/Models/Signature.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace BookonnectAPI.Models;
+
+public class Signature
+{
+	public int ID { get; set; }
+	public DateTime DateTime { get; set; }
+	public bool Signed { get; set; }
+	/*
+	 * A signature must be associated with an OrderVendor
+	 * A signature must be associated/signed off with a User
+	 */
+	public int OrderVendorID { get; set; }
+	public OrderVendor OrderVendor { get; set; } = null!;
+	public int UserID { get; set; }
+	public User User { get; set; } = null!;
+}
+
