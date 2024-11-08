535c02fb9a7a174dc9443eceb56d8b6171183970
Chris Maina <mainachrisw@gmail.com>
2024-11-08 06:57:04 +0300

    drop Deliveries table

--- BookonnectAPI/Data/BookonnectContext.cs
+++ BookonnectAPI/Data/BookonnectContext.cs
@@ -16,7 +16,6 @@ public class BookonnectContext: DbContext
     public DbSet<Image> Images { get; set; }
     public DbSet<Order> Orders { get; set; }
     public DbSet<Payment> Payments { get; set; }
-	public DbSet<Delivery> Deliveries { get; set; }
 	public DbSet<CartItem> CartItems { get; set; }
 }
 
