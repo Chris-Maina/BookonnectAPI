0e14a62b89457065f9811b64f935b095ba39f8ca
Chris Maina <mainachrisw@gmail.com>
2024-11-08 08:14:44 +0300

    add Confirmations table

--- BookonnectAPI/Data/BookonnectContext.cs
+++ BookonnectAPI/Data/BookonnectContext.cs
@@ -17,5 +17,6 @@ public class BookonnectContext: DbContext
     public DbSet<Order> Orders { get; set; }
     public DbSet<Payment> Payments { get; set; }
 	public DbSet<CartItem> CartItems { get; set; }
+    public DbSet<Confirmation> Confirmations { get; set; }
 }
 
---
+++ BookonnectAPI/Models/Confirmation.cs
@@ -0,0 +1,23 @@
+﻿namespace BookonnectAPI.Models;
+
+public enum ConfirmationType
+{
+	Dispatch,
+	Receipt,
+	Cancel
+}
+public class Confirmation
+{
+	public int ID { get; set; }
+	public DateTime DateTime { get; set; }
+	public ConfirmationType Type { get; set; }
+    /*
+	 * A confirmation must be associated with an OrderItem
+	 * A confirmation must be associated/signed off with a Vendor and Customer
+	 */
+    public int OrderItemID { get; set; }
+	public OrderItem OrderItem { get; set; } = null!;
+	public int UserID { get; set; }
+	public User User { get; set; } = null!;
+}
+
