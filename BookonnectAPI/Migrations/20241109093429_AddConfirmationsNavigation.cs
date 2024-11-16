59ca4fa91dd800bf5ca9768ef9f1f76fe1c25fe3
Chris Maina <mainachrisw@gmail.com>
2024-11-16 09:44:50 +0300

    update migrations with Confirmations

---
+++ BookonnectAPI/Migrations/20241109093429_AddConfirmationsNavigation.cs
@@ -0,0 +1,168 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookonnectAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddConfirmationsNavigation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Confirmations_OrderItem_OrderItemID",
+                table: "Confirmations");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderItem_Books_BookID",
+                table: "OrderItem");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderItem_Orders_OrderID",
+                table: "OrderItem");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderItem_Users_CustomerID",
+                table: "OrderItem");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_OrderItem",
+                table: "OrderItem");
+
+            migrationBuilder.DropIndex(
+                name: "IX_OrderItem_CustomerID",
+                table: "OrderItem");
+
+            migrationBuilder.DropColumn(
+                name: "CustomerID",
+                table: "OrderItem");
+
+            migrationBuilder.RenameTable(
+                name: "OrderItem",
+                newName: "OrderItems");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_OrderItem_OrderID",
+                table: "OrderItems",
+                newName: "IX_OrderItems_OrderID");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_OrderItem_BookID",
+                table: "OrderItems",
+                newName: "IX_OrderItems_BookID");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_OrderItems",
+                table: "OrderItems",
+                column: "ID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Confirmations_OrderItems_OrderItemID",
+                table: "Confirmations",
+                column: "OrderItemID",
+                principalTable: "OrderItems",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderItems_Books_BookID",
+                table: "OrderItems",
+                column: "BookID",
+                principalTable: "Books",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderItems_Orders_OrderID",
+                table: "OrderItems",
+                column: "OrderID",
+                principalTable: "Orders",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Confirmations_OrderItems_OrderItemID",
+                table: "Confirmations");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderItems_Books_BookID",
+                table: "OrderItems");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderItems_Orders_OrderID",
+                table: "OrderItems");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_OrderItems",
+                table: "OrderItems");
+
+            migrationBuilder.RenameTable(
+                name: "OrderItems",
+                newName: "OrderItem");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_OrderItems_OrderID",
+                table: "OrderItem",
+                newName: "IX_OrderItem_OrderID");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_OrderItems_BookID",
+                table: "OrderItem",
+                newName: "IX_OrderItem_BookID");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CustomerID",
+                table: "OrderItem",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_OrderItem",
+                table: "OrderItem",
+                column: "ID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderItem_CustomerID",
+                table: "OrderItem",
+                column: "CustomerID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Confirmations_OrderItem_OrderItemID",
+                table: "Confirmations",
+                column: "OrderItemID",
+                principalTable: "OrderItem",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderItem_Books_BookID",
+                table: "OrderItem",
+                column: "BookID",
+                principalTable: "Books",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderItem_Orders_OrderID",
+                table: "OrderItem",
+                column: "OrderID",
+                principalTable: "Orders",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderItem_Users_CustomerID",
+                table: "OrderItem",
+                column: "CustomerID",
+                principalTable: "Users",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
