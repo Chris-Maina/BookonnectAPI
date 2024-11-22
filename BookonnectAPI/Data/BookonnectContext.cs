d22afa235189e278f3d30a2e22d67e24ec92d8f2
Chris Maina <mainachrisw@gmail.com>
2024-11-22 10:58:36 +0300

    seed DB with admin user

--- BookonnectAPI/Data/BookonnectContext.cs
+++ BookonnectAPI/Data/BookonnectContext.cs
@@ -1,16 +1,20 @@
-﻿using BookonnectAPI.Models;
+﻿using BookonnectAPI.Configuration;
+using BookonnectAPI.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 
 namespace BookonnectAPI.Data;
 
 public class BookonnectContext: DbContext
 {
-	public BookonnectContext(DbContextOptions<BookonnectContext> options)
+    private MailSettingsOptions _mailSettings;
+    public BookonnectContext(DbContextOptions<BookonnectContext> options, IOptionsSnapshot<MailSettingsOptions> mailSettings)
 		: base(options)
 	{
+        _mailSettings = mailSettings.Value;
 	}
 
-	public DbSet<User> Users { get; set; }
+    public DbSet<User> Users { get; set; }
     public DbSet<Account> Accounts { get; set; }
     public DbSet<Book> Books { get; set; }
     public DbSet<Image> Images { get; set; }
@@ -19,5 +23,11 @@ public class BookonnectContext: DbContext
     public DbSet<Payment> Payments { get; set; }
 	public DbSet<CartItem> CartItems { get; set; }
     public DbSet<Confirmation> Confirmations { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>().HasData(
+            new User { ID = 1, Name = _mailSettings.Name, Email = _mailSettings.EmailId, Image = "", Phone = "" });
+    }
 }
 
