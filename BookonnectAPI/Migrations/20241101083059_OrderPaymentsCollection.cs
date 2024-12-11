1f3ede381a07e1789ab9d620755bf6a78752817e
Chris Maina <krismaish34@gmail.com>
2024-12-11 10:02:18 +0300

    modify OrderPaymentsCollection migration

--- BookonnectAPI/Migrations/20241101083059_OrderPaymentsCollection.cs
+++ BookonnectAPI/Migrations/20241101083059_OrderPaymentsCollection.cs
@@ -10,6 +10,10 @@ namespace BookonnectAPI.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Payments_Orders_OrderID",
+                table: "Payments");
+
             migrationBuilder.DropIndex(
                 name: "IX_Payments_OrderID",
                 table: "Payments");
@@ -21,6 +25,14 @@ namespace BookonnectAPI.Migrations
             migrationBuilder.DropColumn(
                 name: "OrderId",
                 table: "Books");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Payments_Orders_OrderID",
+                table: "Payments",
+                column: "OrderID",
+                principalTable: "Orders",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
 
             migrationBuilder.CreateIndex(
                 name: "IX_Payments_OrderID",
@@ -54,6 +66,10 @@ namespace BookonnectAPI.Migrations
                 name: "FK_Deliveries_Users_UserID",
                 table: "Deliveries");
 
+            migrationBuilder.DropForeignKey(
+               name: "FK_Payments_Orders_OrderID",
+               table: "Payments");
+
             migrationBuilder.DropIndex(
                 name: "IX_Payments_OrderID",
                 table: "Payments");
@@ -71,6 +87,14 @@ namespace BookonnectAPI.Migrations
                 table: "Books",
                 type: "INTEGER",
                 nullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Payments_Orders_OrderID",
+                table: "Payments",
+                column: "OrderID",
+                principalTable: "Orders",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
 
             migrationBuilder.CreateIndex(
                 name: "IX_Payments_OrderID",
