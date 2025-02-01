fc367f54f973f11968f0b04518ad4fc407abc426
Chris Maina <krismaish34@gmail.com>
2025-02-01 20:18:30 +0300

    modify to use NOW() function

--- BookonnectAPI/Migrations/20250131180214_AddInventoryLogsAndQuantityProperty.cs
+++ BookonnectAPI/Migrations/20250131180214_AddInventoryLogsAndQuantityProperty.cs
@@ -25,7 +25,7 @@ namespace BookonnectAPI.Migrations
                     ID = table.Column<int>(type: "INTEGER", nullable: false)
                         .Annotation("Sqlite:Autoincrement", true),
                     Quantity = table.Column<int>(type: "INTEGER", nullable: false),
-                    DateTime = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETDATE()"),
+                    DateTime = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "NOW()"),
                     Type = table.Column<int>(type: "INTEGER", nullable: false, defaultValue: 0),
                     BookID = table.Column<int>(type: "INTEGER", nullable: false)
                 },
