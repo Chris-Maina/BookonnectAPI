f4474b9c740c6a1764411b92bc6c88be7a069bca
Chris Maina <krismaish34@gmail.com>
2025-02-01 23:55:58 +0300

    comment out parts of migration

--- BookonnectAPI/Migrations/20250131180214_AddInventoryLogsAndQuantityProperty.cs
+++ BookonnectAPI/Migrations/20250131180214_AddInventoryLogsAndQuantityProperty.cs
@@ -11,60 +11,50 @@ namespace BookonnectAPI.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            //migrationBuilder.AddColumn<int>(
-            //    name: "Quantity",
-            //    table: "Books",
-            //    type: "INTEGER",
-            //    nullable: false,
-            //    defaultValue: 1);
+            migrationBuilder.AddColumn<int>(
+                name: "Quantity",
+                table: "Books",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 1);
 
-            //migrationBuilder.CreateTable(
-            //    name: "InventoryLogs",
-            //    columns: table => new
-            //    {
-            //        ID = table.Column<int>(type: "INTEGER", nullable: false)
-            //            .Annotation("Sqlite:Autoincrement", true),
-            //        Quantity = table.Column<int>(type: "INTEGER", nullable: false),
-            //        DateTime = table.Column<DateTime>(type: "timestamp", nullable: false),
-            //        Type = table.Column<int>(type: "INTEGER", nullable: false, defaultValue: 0),
-            //        BookID = table.Column<int>(type: "INTEGER", nullable: false)
-            //    },
-            //    constraints: table =>
-            //    {
-            //        table.PrimaryKey("PK_InventoryLogs", x => x.ID);
-            //        table.ForeignKey(
-            //            name: "FK_InventoryLogs_Books_BookID",
-            //            column: x => x.BookID,
-            //            principalTable: "Books",
-            //            principalColumn: "ID",
-            //            onDelete: ReferentialAction.Cascade);
-            //    });
+            migrationBuilder.CreateTable(
+                name: "InventoryLogs",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Quantity = table.Column<int>(type: "INTEGER", nullable: false),
+                    DateTime = table.Column<DateTime>(type: "timestamp", nullable: false),
+                    Type = table.Column<int>(type: "INTEGER", nullable: false, defaultValue: 0),
+                    BookID = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_InventoryLogs", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_InventoryLogs_Books_BookID",
+                        column: x => x.BookID,
+                        principalTable: "Books",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
 
-            //migrationBuilder.CreateIndex(
-            //    name: "IX_InventoryLogs_BookID",
-            //    table: "InventoryLogs",
-            //    column: "BookID");
-
-            migrationBuilder.Sql(@"
-                 INSERT INTO InventoryLogs(Quantity, BookID, DateTime)
-                 SELECT Quantity, ID, NOW()
-                 FROM Books;
-                ");
+            migrationBuilder.CreateIndex(
+                name: "IX_InventoryLogs_BookID",
+                table: "InventoryLogs",
+                column: "BookID");
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.Sql(@"
-                DELETE * FROM InventoryLogs;
-            ");
+            migrationBuilder.DropTable(
+                name: "InventoryLogs");
 
-            //migrationBuilder.DropTable(
-            //    name: "InventoryLogs");
-
-            //migrationBuilder.DropColumn(
-            //    name: "Quantity",
-            //    table: "Books");
+            migrationBuilder.DropColumn(
+                name: "Quantity",
+                table: "Books");
         }
     }
 }
