1854394245b86e366e68eeea8410a83ec16eeced
Chris Maina <krismaish34@gmail.com>
2025-03-08 01:15:39 +0300

    add BooksApiKey

--- BookonnectAPI/Configuration/GoogleOptions.cs
+++ BookonnectAPI/Configuration/GoogleOptions.cs
@@ -4,5 +4,6 @@ public class GoogleOptions
 {
 	public const string SectionName = "Authentication:Google";
 	public string ClientId { get; set;} = string.Empty;
+	public string BooksApiKey { get; set; } = string.Empty;
 }
 
