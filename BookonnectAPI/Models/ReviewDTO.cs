c3227a40b91cc25d3bfebd0d1ce7569192b3bc9e
Chris Maina <krismaish34@gmail.com>
2025-03-12 09:47:16 +0300

    update ReviewDTO

--- BookonnectAPI/Models/ReviewDTO.cs
+++ BookonnectAPI/Models/ReviewDTO.cs
@@ -7,8 +7,6 @@ public class ReviewDTO
     public ReviewStatus Status { get; set; }
     public DateTime DateTime { get; set; }
     public string? Text { get; set; }
-    public int? UserID { get; set; }
-    public User? User { get; set; }
     public int BookID { get; set; }
     public BookDTO? Book { get; set; }
 }
