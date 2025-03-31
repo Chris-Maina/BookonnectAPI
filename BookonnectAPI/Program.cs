2a61a1b0cd2040dfa4310d160e06d781740f5cd4
Chris Maina <krismaish34@gmail.com>
2025-03-31 13:12:53 +0300

    add GeminiService http client

--- BookonnectAPI/Program.cs
+++ BookonnectAPI/Program.cs
@@ -101,6 +101,7 @@ builder.Services.AddHttpClient("Safaricom", httpClient =>
     httpClient.BaseAddress = new Uri("https://sandbox.safaricom.co.ke");
 });
 builder.Services.AddHttpClient<IGoogleBooksApiService, GoogleBooksApiService>();
+builder.Services.AddHttpClient<IGeminiService, GeminiService>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
